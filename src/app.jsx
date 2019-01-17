4f7698efe9ae5a78449c6ab3c0750f2525bb7b4e
ElliottG92 <ejgranoff@gmail.com>
2019-01-16 22:05:07 -0800

    finished inital setup of repo

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>first react component</div>
+        )
+    }
+}
+
+module.exports = App;
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+const ReactDOM = require('react-dom');
+const React = require('react');
+const App = require('./app.jsx')
+
+
+ReactDOM.render( <App />, document.getElementById("root") );
